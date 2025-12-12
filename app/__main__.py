80df0efdaf962ddf437bfaeb10304e3ad8fc696c
Liam Egan <liam@liamegan.com>
2025-12-12 14:47:39 -0500

    Move assets to separate files

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,13 @@
+from pathlib import Path
 from werkzeug.serving import run_simple
 
 from . import app
 
-run_simple("0.0.0.0", 4000, app, use_reloader = True)
+run_simple(
+	"0.0.0.0",
+	4000,
+	app,
+	use_reloader = True,
+	extra_files = [str(Path("app", "assets")), str(Path("app", "views"))],
+	static_files = {"/static": str(Path("public", "static"))}
+)
