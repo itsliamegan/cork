55180d1cf7d0fb39f3cf152d25c6b0ee68e38354
Liam Egan <liam@liamegan.com>
2026-09-03 20:25:22 -0400

    Switch to uv

--- app/__init__.py
+++ app/__init__.py
@@ -30,3 +30,19 @@ app = Application(routes, [
 	Guards(guards),
 ])
 app.boot()
+
+def main():
+	from pathlib import Path
+	from werkzeug.serving import run_simple
+
+	asset_files = [str(file) for file in Path("app", "assets").glob("**/*")]
+	view_files = [str(file) for file in Path("app", "views").glob("**/*.html")]
+
+	run_simple(
+		"0.0.0.0",
+		4000,
+		app,
+		use_reloader = True,
+		extra_files = [*asset_files, *view_files],
+		static_files = {"/static": str(Path("public", "static"))}
+	)
