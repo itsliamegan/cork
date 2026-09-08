0582ab37f3030c0780c1051f8eecc47c3dea2ae6
Liam Egan <liam@liamegan.com>
2026-09-08 16:45:59 -0400

    Create app as subclass of Application

--- app/__init__.py
+++ app/__init__.py
@@ -11,16 +11,32 @@ from helios.wsgi import Application
 from app.data import User, schema
 from app.http import routes
 
-app = Application(
-	Router(routes),
-	[
-		helios.store.Component(Path("data", "store.json"), schema),
-		helios.views.Component(Path("app", "views")),
-		helios.session.Component(Path("data", "sessions.json")),
-		helios.flash.Component(),
-		helios.auth.Component(User),
-	],
-)
+ROOT_DIR = Path(__file__).resolve().parent.parent
+STORE_FILE = ROOT_DIR.joinpath("data", "store.json")
+SESSIONS_FILE = ROOT_DIR.joinpath("data", "sessions.json")
+VIEWS_DIR = ROOT_DIR.joinpath("app", "views")
+
+
+class Application(Application):
+	def __init__(
+		self,
+		store_file: Path = STORE_FILE,
+		sessions_file: Path = SESSIONS_FILE,
+		views_dir: Path = VIEWS_DIR,
+	):
+		super().__init__(
+			Router(routes),
+			[
+				helios.store.Component(store_file, schema),
+				helios.views.Component(views_dir),
+				helios.session.Component(sessions_file),
+				helios.flash.Component(),
+				helios.auth.Component(User),
+			],
+		)
+
+
+app = Application()
 app.boot()
 
 
